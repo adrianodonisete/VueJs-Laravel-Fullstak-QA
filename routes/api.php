38858e111865e201ad630322afc27cf7fe295487
Adriano Machado <adrianodonisete@gmail.com>
2023-07-31 18:02:45 -0300

    Create tasks operations

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\V1\CompleteTaskController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +19,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('/tasks', TaskController::class);
+
+    Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
